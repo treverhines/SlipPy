7c74a8fb297b485a6cf4cb8eb8cf33995e8a82d8
hinest <treverhines@gmail.com>
2016-06-26 17:11:57 -0400

    added better documentation to tikhonov module

--- slippy/tikhonov.py
+++ slippy/tikhonov.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import division
 import numpy as np
 
 def remove_zero_rows(M):
@@ -13,30 +14,46 @@ def remove_zero_rows(M):
   return out
 
 
-def linear_to_array_index(val,shape,wrap=False):
+def flat_to_array_index(idx,shape):
   ''' 
-  used in next method of IndexEnumerate
-  '''
-  if (not wrap) & (abs(val)+(val>0) > np.prod(shape)):
-    raise IndexError(
-      'positive indices must be less than the product of the shape '
-      'and the absolute value of negative indices must be less than '
-      'or equal to the product of the shape. received index %s for '
-      'shape %s' % (val,shape))
+  converts an index for a flattened array to the subscript indices 
+  which call the same element in the unflattened array
 
+  Parameters
+  ----------
+
+    idx : int
+      index for flat array
+
+    shape : tuple
+      shape of folded array
+    
+  '''
   indices = ()
   for dimsize in shape[::-1]:
-    indices += val%dimsize,
-    val = val // dimsize
+    indices += idx%dimsize,
+    idx = idx // dimsize
   return indices[::-1]
 
 
 class Perturb:
-  def __init__(self,v,delta=1,cast=np.asarray):
+  ''' 
+  Iterator which takes an iterable, V, and returns Vk on iteration k, 
+  where Vk is defined as:
+  
+    V[i] + delta     if i = k
+    V[i]             else
+  
+  Parameters
+  ----------
+    v : iterable
+    delta : optional
+
+  '''
+  def __init__(self,v,delta=1):
     self.v = v
-    self.cast = cast
     self.N = len(v)
-    self.d = delta
+    self.delta = delta
     self.k = 0 
 
   def __iter__(self):
@@ -51,29 +68,26 @@ class Perturb:
       raise StopIteration
 
     else:
-      out = [j if (i != self.k) else (j + self.d) 
-             for i,j in enumerate(self.v)]   
+      out = [j if (i != self.k) else (j + self.delta) for i,j in enumerate(self.v)]   
       self.k += 1
-      return self.cast(out)
+      return out
       
 
 class ArrayIndexEnumerate:
   def __init__(self,C):
     ''' 
-    used in tikhonov matrix
+    enumerates over the flattened elements of C and their array indices
 
-    enumerates over the flattened elements of C and their index locations in C
-  
-    e.g.
-  
-    >> C = np.array([[1,2],[3,4]])
-    >> for idx,val in IndexEnumerate(C):
-    ...  print('idx: %s, val: %s' % (idx,val))   
-    ...
-    idx: [0, 0], val: 1
-    idx: [0, 1], val: 2
-    idx: [1, 0], val: 3
-    idx: [1, 1], val: 4
+    Example
+    -------
+      >> C = np.array([[1,2],[3,4]])
+      >> for idx,val in ArrayIndexEnumerate(C):
+      ...  print('idx: %s, val: %s' % (idx,val))   
+      ...
+      idx: (0, 0), val: 1
+      idx: (0, 1), val: 2
+      idx: (1, 0), val: 3
+      idx: (1, 1), val: 4
     '''
     self.C = np.asarray(C)
     self.itr = 0
@@ -90,41 +104,151 @@ class ArrayIndexEnumerate:
       raise StopIteration
 
     else:
-      idx = linear_to_array_index(self.itr,self.C.shape)
+      idx = flat_to_array_index(self.itr,self.C.shape)
       self.itr += 1
       return (idx,self.C[idx])
 
 
-class Neighbors(ArrayIndexEnumerate):
+class ForwardNeighbors:
   ''' 
-  Iterator that Loops over elements in array C returning the element and its  
-  neighbors
+  iterates over the flattened elements of C, returning the element and 
+  its forward looking neighbors for each axis.
+    
+  Example
+  -------
+    >> C = np.array([[1,2,3],
+                     [4,5,6]])  
+    >> for i in ForwardNeighbors(C):
+         print(i)
+    [4,2],1
+    [5,3],2
+    [6],3
+    [5],4
+    [6],5
+    [],6
   '''
-  def __init__(self,C,search='all'):
-    ArrayIndexEnumerate.__init__(self,C)
-    assert search in ['all','forward','backward']
-    self.search = search
-
+  def __init__(self,C):
+    self.C = np.asarray(C)
+    self.itr = 0
+    
   def next(self):
     return self.__next__()
-
-  def __next__(self):
-    idx,val = ArrayIndexEnumerate.__next__(self)
-    neighbors = np.zeros(0,dtype=self.C.dtype)
-    if (self.search == 'all') | (self.search == 'forward'):
-      for pert in Perturb(idx,1,cast=tuple):
-        if any(i>=j for i,j in zip(pert,self.C.shape)):
-          continue
-        neighbors = np.append(neighbors,self.C[pert])
-
-    if (self.search == 'all') | (self.search == 'backward'):
-      for pert in Perturb(idx,-1,cast=tuple):
-        if any(i<0 for i in pert):
-          continue
-        neighbors = np.append(neighbors,self.C[pert])
     
-    return neighbors,val
+  def __iter__(self):
+    return self
 
+  def __next__(self):  
+    if self.itr == self.C.size:
+      raise StopIteration
+    
+    else:
+      out = []
+      idx = flat_to_array_index(self.itr,self.C.shape)
+      for idx_pert in Perturb(idx,1):
+        # this checks if the perturbed indices are valid for C
+        if any(i>=j for i,j in zip(idx_pert,self.C.shape)):
+          continue
+        out += [self.C[tuple(idx_pert)]] 
+
+      self.itr += 1
+      return out,self.C[idx]
+      
+
+class BackwardNeighbors:
+  ''' 
+  iterates over the flattened elements of C, returning the element and 
+  its backward looking neighbors for each axis.
+    
+  Example
+  -------
+    >> C = np.array([[1,2,3],
+                     [4,5,6]])  
+    >> for i in ForwardNeighbors(C):
+         print(i)
+    [],1
+    [1],2
+    [2],3
+    [1],4
+    [2,4],5
+    [3,5],6
+  '''
+  def __init__(self,C):
+    self.C = np.asarray(C)
+    self.itr = 0
+    
+  def next(self):
+    return self.__next__()
+    
+  def __iter__(self):
+    return self
+
+  def __next__(self):  
+    if self.itr == self.C.size:
+      raise StopIteration
+    
+    else:
+      out = []
+      idx = flat_to_array_index(self.itr,self.C.shape)
+      for idx_pert in Perturb(idx,-1):
+        # this checks if the perturbed indices are valid for C
+        if any(i<0 for i in idx_pert):
+          continue
+        out += [self.C[tuple(idx_pert)]] 
+
+      self.itr += 1
+      return out,self.C[idx]
+      
+
+class Neighbors:      
+  ''' 
+  iterates over the flattened elements of C, returning the element and 
+  its forward and backward looking neighbors for each axis.
+    
+  Example
+  -------
+    >> C = np.array([[1,2,3],
+                     [4,5,6]])  
+    >> for i in ForwardNeighbors(C):
+         print(i)
+    [4,2],1
+    [5,3,1],2
+    [6,2],3
+    [5,1],4
+    [6,2,4],5
+    [3,5],6
+  '''
+  def __init__(self,C):
+    self.C = np.asarray(C)
+    self.itr = 0
+    
+  def next(self):
+    return self.__next__()
+    
+  def __iter__(self):
+    return self
+
+  def __next__(self):  
+    if self.itr == self.C.size:
+      raise StopIteration
+    
+    else:
+      out = []
+      idx = flat_to_array_index(self.itr,self.C.shape)
+      for idx_pert in Perturb(idx,1):
+        # this checks if the perturbed indices are valid for C
+        if any(i>=j for i,j in zip(idx_pert,self.C.shape)):
+          continue
+        out += [self.C[tuple(idx_pert)]] 
+
+      for idx_pert in Perturb(idx,-1):
+        # this checks if the perturbed indices are valid for C
+        if any(i<0 for i in idx_pert):
+          continue
+        out += [self.C[tuple(idx_pert)]] 
+
+      self.itr += 1
+      return out,self.C[idx]
+      
 
 def _tikhonov_zeroth_order(C,L):
   ''' 
@@ -143,7 +267,7 @@ def _tikhonov_first_order(C,L):
   used in tikhonov_matrix
   '''
   Lrow = 0
-  for neighbors,i in Neighbors(C,'forward'):
+  for neighbors,i in ForwardNeighbors(C):
     if i == -1:
       continue
     for k in neighbors:
@@ -160,46 +284,53 @@ def _tikhonov_second_order(C,L):
   ''' 
   used in tikhonov_matrix
   '''
-  for neighbors,i in Neighbors(C,'all'):
+  for nbrs,i in Neighbors(C):
     if i == -1:
       continue
-    order = sum(neighbors != -1)
-    for k in neighbors:
+    denom = len([n for n in nbrs if n != -1])
+    for k in nbrs:
       if k == -1:
         continue
-      L[i,i] += -1.0/order  
-      L[i,k]  +=  1.0/order
+      L[i,i] += -1.0/denom 
+      L[i,k]  +=  1.0/denom
 
   return L
 
 
-def tikhonov_matrix(C,n,column_no=None,dtype=None):
+def tikhonov_matrix(C,order,column_no=None):
   ''' 
   Parameters
   ----------
-    C: connectivity matrix, this can contain '-1' elements which can be used
-       to break connections. 
-    n: order of tikhonov regularization
-    column_no: number of columns in the output matrix
-    sparse_type: either 'bsr','coo','csc','csr','dia','dok','lil'
+    C: int array
+      connectivity matrix, this can contain '-1' elements which can be used
+      to break connections. 
+      
+    order: int
+      order of tikhonov regularization. can be either 0, 1, or 2
+
+    column_no: int
+      number of columns in the output matrix
 
   Returns
   -------
-    L: tikhonov regularization matrix saved as a csr sparse matrix
+    L: tikhonov regularization matrix 
 
   Example
   -------
-    first order regularization for 4 model parameters which are related in 2D 
-    space
-      >> Connectivity = [[0,1],[2,3]]
-      >> L = tikhonov_matrix(Connectivity,1)
-      
+    >> Connectivity = [[0,1],[2,3]]
+    >> L = tikhonov_matrix(Connectivity,1)
+
+    array([[-1.,  0.,  1.,  0.],
+           [-1.,  1.,  0.,  0.],
+           [ 0., -1.,  0.,  1.],
+           [ 0.,  0., -1.,  1.]])
+
   '''     
-  C = np.array(C)
+  C = np.asarray(C,dtype=int)
   # check to make sure all values (except -1) are unique
   idx = C != -1
   params = C[idx] 
-  unique_params = set(params)
+  unique_params = np.unique(params)
   assert len(params) == len(unique_params), (
          'all values in C, except for -1, must be unique')
 
@@ -217,16 +348,16 @@ def tikhonov_matrix(C,n,column_no=None,dtype=None):
   assert column_no >= max_param, (
          'column_no must be at least as large as max(C)')
 
-  if n == 0:
-    L = np.zeros((column_no,column_no),dtype=dtype)
+  if order == 0:
+    L = np.zeros((column_no,column_no))
     L =  _tikhonov_zeroth_order(C,L)
 
-  if n == 1:
-    L = np.zeros((Cdim*column_no,column_no),dtype=dtype)
+  if order == 1:
+    L = np.zeros((Cdim*column_no,column_no))
     L = _tikhonov_first_order(C,L)
 
-  if n == 2:
-    L = np.zeros((column_no,column_no),dtype=dtype)
+  if order == 2:
+    L = np.zeros((column_no,column_no))
     L = _tikhonov_second_order(C,L)
 
   L = remove_zero_rows(L)     
